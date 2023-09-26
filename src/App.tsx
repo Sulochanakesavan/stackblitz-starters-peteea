e27bc4c1542e638a7be1418bde9905af24ba1b47
Sulochanakesavan <115818111+Sulochanakesavan@users.noreply.github.com>
2023-09-26 14:52:29 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+// import { FC } from 'react';
+
+// import './style.css';
+
+// export const App: FC<{ name: string }> = ({ name }) => {
+//   return (
+//     <div>
+//       <h1>Hello {name}!</h1>
+//       <p>Start editing to see some magic happen :)</p>
+//     </div>
+//   );
+// };
+
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
+  );
+}
+
+export default App;
